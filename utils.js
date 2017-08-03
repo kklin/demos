ae7e2e7787dcc1730aba1889069a813d8d3434ac
Kevin Lin <kevin@quilt.io>
2017-08-03 16:54:29 -0700

    demos: Lint all files

--- utils.js
+++ utils.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 
-exports.addSshKey = function(machine) {
+exports.addSshKey = function addSshKey(machine) {
   // Try to get a SSH public key to use by looking for one in ~/.ssh/id_rsa.pub.
   const publicKeyFile = path.join(process.env.HOME, '.ssh/id_rsa.pub');
   if (fs.existsSync(publicKeyFile)) {
@@ -11,9 +9,8 @@ exports.addSshKey = function(machine) {
     machine.sshKeys.push(sshPublicKey);
   } else {
     console.warn(`No SSH public key found in ${publicKeyFile}. ` +
-        `Machines will still be launched, but will not allow SSH acesss. ` +
-        `If you'd like to enable SSH access, use the ssh-keygen command to ` +
-        `generate a public SSH key and then re-run this blueprint.`);
+        'Machines will still be launched, but will not allow SSH acesss. ' +
+        'If you\'d like to enable SSH access, use the ssh-keygen command to ' +
+        'generate a public SSH key and then re-run this blueprint.');
   }
-}
-
+};
