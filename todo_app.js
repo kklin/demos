ae399f14df02526a3f075e0f018a54d197c8d419
Kevin Lin <kevin@quilt.io>
2017-08-03 16:54:29 -0700

    demos: Lint all files

--- todo_app.js
+++ todo_app.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const haproxy = require('@quilt/haproxy');
 const Mongo = require('@quilt/mongo');
 const Node = require('@quilt/nodejs');
-const {publicInternet} = require('@quilt/quilt');
+const { publicInternet } = require('@quilt/quilt');
 
 function TodoApp(count) {
   const port = 80;
@@ -16,16 +14,21 @@ function TodoApp(count) {
       MONGO_URI: this.mongo.uri('mean-example'),
     },
   });
+
+  // The Node module isn't designed well for composition right now, so we have
+  // to access this.app._app directly. Until this is fixed, silence the linter
+  // warning about underscore dangles.
+  // eslint-disable-next-line no-underscore-dangle
   this.proxy = haproxy.singleServiceLoadBalancer(1, this.app._app);
 
   this.app.connect(this.mongo.port, this.mongo);
   this.proxy.allowFrom(publicInternet, haproxy.exposedPort);
 
-  this.deploy = function(deployment) {
+  this.deploy = function deploy(deployment) {
     deployment.deploy(this.app);
     deployment.deploy(this.mongo);
     deployment.deploy(this.proxy);
-  }
+  };
 }
 
 module.exports = TodoApp;
